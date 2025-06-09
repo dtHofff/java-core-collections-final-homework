28657c218dac428c615d60efacd9154901ac21e1
dtHofff <dmitriybru@gmail.com>
2025-06-09 14:27:50 +0300

    Created Contact.java, made constructor and override equals and hashcode methods.

---
+++ src/main/java/school/sorokin/javacore/Contact.java
@@ -0,0 +1,35 @@
+package school.sorokin.javacore;
+
+import java.util.Objects;
+
+public class Contact {
+    private final String name;
+    private final String phone;
+    private final String email;
+    private final String group;
+
+    public Contact(String name, String phone, String email, String group) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.group = group;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Оптимизация: если ссылки совпадают, объекты равны
+        if (this == obj) return true;
+        // Если объект null или принадлежит другому классу, возвращаем false
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Contact other = (Contact) obj;
+
+        if (!Objects.equals(phone, other.phone)) return false;
+        return (name != null ? name.equals(other.name) : other.name == null);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phone);
+    }
+}
